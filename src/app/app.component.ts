ca9b35f59741cd1c00e2d19b7b531fc5cf83c197
Your Name <you@example.com>
2021-09-02 14:18:44 +0800

    feat:新增项目

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+@Component({//装饰器，为组件指定所需元数据
+  selector: 'app-root',//组件选择器（css元素选择器）
+  templateUrl: './app.component.html',//组件模板的位置
+  styleUrls: ['./app.component.less']//组件私有的css样式文件位置
 })
-export class AppComponent {
-  title = 'angular-tour-of-heroes';
+export class AppComponent { //导出组件类
+  title = 'Tour of Heroes';
 }
